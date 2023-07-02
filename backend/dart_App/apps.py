2fc1fc86170f26e6ee1b27481f51b4a434e66abe
rohanraj7 <rohanraj9645@gmail.com>
2023-07-03 00:29:21 +0530

    backend connection build created

---
+++ backend/dart_App/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DartAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dart_App'
---
+++ backend/dart_App/views.py
@@ -0,0 +1,8 @@
+from urllib import response
+from django.shortcuts import render
+
+# Create your views here.
+
+
+def home(request):
+    return response("HELLOO AMAL")
